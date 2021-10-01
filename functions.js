cac017ba0101211f4ec9ca93b566cf44bcb187cf
jackiewu99 <Jackie.Wu99@myhunter.cuny.edu>
2021-10-01 18:09:29 -0400

    finished myMap

--- functions.js
+++ functions.js
@@ -7,3 +7,14 @@ Array.prototype.myEach = function(callbackFn) {
         callbackFn(this[i],i, this);
     }
 };
+
+// MAP //
+Array.prototype.myMap = function(callbackFn) {
+    let arrMap = [...this];
+    for(let i = 0;i < arrMap.length;i++)
+    {
+        if(arrMap[i] === undefined) continue;
+        arrMap[i] = callbackFn(this[i],i, this);
+    }
+    return arrMap;
+};
